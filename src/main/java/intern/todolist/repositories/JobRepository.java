8dc239c5d284ceb5641d8392de6ccb2ae9b6539e
Quangdai2207 <daitran.inbox@gmail.com>
2026-07-09 20:39:42 +0700

    fix controller

--- src/main/java/intern/todolist/repositories/JobRepository.java
+++ src/main/java/intern/todolist/repositories/JobRepository.java
@@ -3,6 +3,7 @@ package intern.todolist.repositories;
 import intern.todolist.entities.Job;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -16,10 +17,10 @@ public class JobRepository {
     public JobRepository() {
         jobs = new ArrayList<>(
                 List.of(
-                        new Job(1, "Reading", "Reading 10 pages on a book per day", null, null, false, LocalDateTime.now(), null, false),
-                        new Job(2, "Apply CV", "2 CV per day", null, null, false, LocalDateTime.now(), null, false),
-                        new Job(3, "Learn skill", "Understanding one skill per day", null, null, false, LocalDateTime.now(), null, false)));
-    }
+                        new Job(1, "Job 1", "Do something...", LocalDate.now(), LocalDate.now(), false, LocalDateTime.now(), null, false),
+                        new Job(2, "Job 2", "Do something...", LocalDate.now(), LocalDate.now(), false, LocalDateTime.now(), null, false),
+                        new Job(3, "Job 3", "Do something...", LocalDate.now(), LocalDate.now(), false, LocalDateTime.now(), null, false)));
+    };
 
     // Todo-list: Lay tong so records tao Id tang dan co Job moi
     public int total() {
